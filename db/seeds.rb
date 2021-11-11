cf5a3c0d4853f4d184450f3ba9ae16413f3f2b9b
Kato De Lentacker <kato.delentacker@outlook.com>
2021-11-11 07:08:07 +0100

    working routes, controllers and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+url = open('http://tmdb.lewagon.com/movie/top_rated')
+movie_library = JSON.parse(url.read)
+
+movie_library["results"].each do |movie_hash|
+  Movie.create(title: movie_hash['title'], overview: movie_hash['overview'], poster_url: "http://image.tmdb.org/t/p/w300/#{movie_hash['poster_path']}", rating: movie_hash['vote_average'])
+end
+
+List.create(name: 'favorites')
+List.create(name: 'watched')
+List.create(name: 'Not watched yet')
